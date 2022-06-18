99dbb91261e3a148d782e9f57c22648119cf7062
Zclark0224 <zclark0224@gmail.com>
2022-06-18 11:49:29 -0400

    make code dry

--- index.js
+++ index.js
@@ -4,15 +4,12 @@ const movieTitleContainer = document.getElementById("movie-title-container")
 const watchlistBtn = document.getElementsByClassName("movie-button")
 const watchlistMovieTitleContainer = document.getElementById("watchlist-movie-title-container")
 const pickForMeBtn = document.getElementById("pick-for-me-btn")
-const popup = document.querySelector(".popup")
-const closePopup = document.querySelector(".close-popup")
+const popup = document.getElementById("popup")
+const popupContents = document.getElementById("popup-contents")
 
-//global variable for all searched movies
-let movieIds = []
-let watchlistMovies = []
-
+//search button on index.html
 if(searchSubmitBtn){
-    searchSubmitBtn.addEventListener("click", getMovieData)
+    searchSubmitBtn.addEventListener("click", getMovieSearch)
     searchTermInput.addEventListener("keypress", function(event){
         if(event.key === "Enter"){
             event.preventDefault()
@@ -21,6 +18,7 @@ if(searchSubmitBtn){
     })
 }
 
+//pick for me button on watchlist.html
 if(pickForMeBtn){
     if(window.localStorage.length == 0){
         pickForMeBtn.style.display = 'none'
@@ -30,19 +28,16 @@ if(pickForMeBtn){
     pickForMeBtn.addEventListener("click", pickRandomMovie)
 }
 
-async function getMovieData(){
-    movieIds = []
-
-    //get search list
-    const movieTitle = searchTermInput.value.toLowerCase().split(" ").join("_")
-    const searchListRes = await fetch(`http://www.omdbapi.com/?apikey=6f50b1e3&s=${movieTitle}`)
+async function getMovieSearch(){
+    //get search list from api
+    const movieTitleSearch = searchTermInput.value.toLowerCase().split(" ").join("_")
+    const searchListRes = await fetch(`http://www.omdbapi.com/?apikey=6f50b1e3&s=${movieTitleSearch}`)
     const searchListData = await searchListRes.json()
 
+    //return either error message or movies
     if(searchListData.Response === "False"){
         movieList = `<div>Unable to find what you're looking for. Please try another search.</div>`
     } else {
-
-        //Move this into it's own function?
         movieList = "";
         for(let index = 0; index < searchListData.Search.length; index++){
             //fetch movie data
@@ -50,73 +45,73 @@ async function getMovieData(){
             const titleResponse = await fetch(`http://www.omdbapi.com/?apikey=6f50b1e3&i=${movieId}`)
             const movieData = await titleResponse.json()
 
-            movieIds.push(movieData)
-
-            //creating movie divs
-            if(movieData.Response === "True" && (movieData.Type == "movie" || movieData.Type == "series")){
-                
-                //no rating placeholder
-                let rating = ""
-                if(movieData.Ratings.length === 0){
-                    rating = "0/10"
-                } else {
-                    rating = `${movieData.Ratings[0].Value}`
-                }
-
-                let button = ``
-                if(window.localStorage.getItem(`${movieData.imdbID}`)){
-                    button = `
-                        <button class="movie-button" onclick="removeSearched('${movieData.Title}', ${index})"><i class="fa-solid fa-circle-minus"> Remove</i></button>
-                        `
-                } else {
-                    button = `
-                        <button class="movie-button" id="${index}btn" onclick="addToWatchlist(${index})"><i class="fa-solid fa-circle-plus"> Watchlist</i></button>
-                        `
-                }
-
-                movieList += `
-                    <div class="movie">
-                        <img src="${movieData.Poster}">
-                        <div class="movie-info-container">
-                            <div class="movie-title-container">
-                                <p class="movie-title">${movieData.Title}</p>
-                                <p class="movie-stars">⭐${rating}</p>
-                            </div>
-                            <div class="movie-subtext-container">
-                                <p class="movie-runtime">${movieData.Runtime}</p>
-                                <p class="movie-genre">${movieData.Genre}</p>
-                                <div class="movie-button-container" id="button-container${index}">
-                                    ${button}
-                                </div>
-                            </div>
-                            <p class="movie-plot">${movieData.Plot}</p>
-                        </div>
-                    </div>
-                    <hr>
-                    `
-            }
+            createMovieDivs(movieData)
         }
     }
     movieTitleContainer.innerHTML = movieList
 }
 
-function addToWatchlist(index) {
-    let key = movieIds[index].imdbID
-    window.localStorage.setItem(`${key}`, JSON.stringify(movieIds[index]))
-    document.getElementById(`button-container${index}`).innerHTML = `
-        <button class="movie-button" onclick="removeSearched('${key}', ${index})"><i class="fa-solid fa-circle-minus"> Remove</i></button>
-        `
+async function getMovieData(key){
+    const titleResponse = await fetch(`http://www.omdbapi.com/?apikey=6f50b1e3&i=${key}`)
+    const movieData = await titleResponse.json()
+    return movieData
 }
 
-function removeSearched(key, index) {
+function createMovieDivs(movieData) {
+    //creating movie divs
+    //checking to only add movies and tv shows
+    if(movieData.Response === "True" && (movieData.Type == "movie" || movieData.Type == "series")){
+        //declare the key as imdbID
+        let key = movieData.imdbID
+                    
+        //check for rating
+        let rating = (movieData.Ratings.length ? `${movieData.Ratings[0].Value}` : "0/10")
+
+        //check for which button to display
+        button = buttonDisplay(key)
+        
+        movieList += `
+            <div class="movie">
+                <img src="${movieData.Poster}">
+                <div class="movie-info-container">
+                    <div class="movie-title-container">
+                        <p class="movie-title">${movieData.Title}</p>
+                        <p class="movie-stars">⭐${rating}</p>
+                    </div>
+                    <div class="movie-subtext-container">
+                        <p class="movie-runtime">${movieData.Runtime}</p>
+                        <p class="movie-genre">${movieData.Genre}</p>
+                        <div class="movie-button-container" id="button-container${movieData.imdbID}">
+                            ${button}
+                        </div>
+                    </div>
+                    <p class="movie-plot">${movieData.Plot}</p>
+                </div>
+            </div>
+            <hr>
+            `
+    }
+}
+
+function buttonDisplay(key){
+    return window.localStorage.getItem(`${key}`)
+        ? `<button class="movie-button" onclick="removeSearched('${key}')"><i class="fa-solid fa-circle-minus"> Remove</i></button>`
+        : `<button class="movie-button" id="${key}btn" onclick="addToWatchlist('${key}')"><i class="fa-solid fa-circle-plus"> Watchlist</i></button>`
+}
+
+async function addToWatchlist(key) {
+    movieData = await getMovieData(key)
+    window.localStorage.setItem(`${key}`, JSON.stringify(movieData))
+    document.getElementById(`button-container${key}`).innerHTML = buttonDisplay(key)
+}
+
+async function removeSearched(key) {
     window.localStorage.removeItem(key)
-    document.getElementById(`button-container${index}`).innerHTML = `
-        <button class="movie-button" id="${index}btn" onclick="addToWatchlist(${index})"><i class="fa-solid fa-circle-plus"> Watchlist</i></button>
-        `
+    document.getElementById(`button-container${key}`).innerHTML = buttonDisplay(key)
 }
 
 function renderWatchlist() {
-    let movieList = ""
+    movieList = ""
 
     if(window.localStorage.length == 0){
         movieList = `
@@ -127,26 +122,7 @@ function renderWatchlist() {
         const keys = Object.keys(localStorage);
         for (let key of keys) {
             let currentKey = JSON.parse(localStorage.getItem(key))
-            movieList += `
-                <div class="movie">
-                    <img src="${currentKey.Poster}">
-                    <div class="movie-info-container">
-                        <div class="movie-title-container">
-                            <p class="movie-title">${currentKey.Title}</p>
-                            <p class="movie-stars">⭐${currentKey.Ratings[0].Value}</p>
-                        </div>
-                        <div class="movie-subtext-container">
-                            <p class="movie-runtime">${currentKey.Runtime}</p>
-                            <p class="movie-genre">${currentKey.Genre}</p>
-                            <div class="movie-button-container" id="button-container}">
-                                <button class="movie-button" onclick="removeFromWatchlist('${key}')"><i class="fa-solid fa-circle-minus"> Remove</i></button>
-                            </div>
-                        </div>
-                        <p class="movie-plot">${currentKey.Plot}</p>
-                    </div>
-                </div>
-                <hr>
-                `
+            createMovieDivs(currentKey)
         }
     }
     watchlistMovieTitleContainer.innerHTML = movieList
@@ -163,8 +139,8 @@ function pickRandomMovie(){
         let randomKey = keys[Math.floor(Math.random() * keys.length)]
         let currentMovie = JSON.parse(localStorage.getItem(randomKey))
         popup.style.display = "block"
-    
-        document.getElementById("popup-contents").innerHTML = `
+
+        popupContents.innerHTML = `
             <div class="movie">
             <img class="popup-img" src="${currentMovie.Poster}">
             <div class="movie-info-container">
